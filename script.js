cbe548972239fb76676afbe92966cd2bae7295ed
gragas <180623g3h34htctrl@users.noreply.github.com>
2024-11-01 09:58:01 +0100

    added while loop + tasks

--- script.js
+++ script.js
@@ -2,31 +2,52 @@ console.log('hello');
 
 let todoTasks = [];
 let completedTasks = [];
+let taskId = 0;
 
-function addTasks() {
-  todoTasks.push(prompt('Lägg till en uppgift'));
-  console.log(todoTasks);
-}
+let completed = true;
 
-let chosenOption = prompt(`
+let descriptionTask;
+
+// const todo = {
+//     id:
+//     description:
+//     done:
+// }
+while (completed) {
+  function addTasks(descriptionTask) {
+    const todo = {
+      id: taskId++,
+      description: descriptionTask,
+      done: false,
+    };
+
+    todoTasks.push(todo);
+    console.log(todoTasks);
+  }
+
+  let chosenOption = prompt(`
     1. Lägg till nya uppgifter
     2. Visa alla aktuella uppgifter.
     3. Markera uppgifter som klara.
     4. Ta bort uppgifter.
     5. Avsluta programmet`);
 
-switch (chosenOption) {
-  case '1':
-    addTasks();
-    break;
-  case '2':
-    break;
-  case '3':
-    break;
-  case '4':
-    break;
-  case '5':
-    break;
-  default:
-    alert('error');
+  switch (chosenOption) {
+    case '1':
+      // todoTasks.push
+      descriptionTask = prompt('Lägg till en uppgift');
+      addTasks(descriptionTask);
+      break;
+    case '2':
+      break;
+    case '3':
+      break;
+    case '4':
+      break;
+    case '5':
+      completed = false;
+      break;
+    default:
+      alert('error');
+  }
 }
